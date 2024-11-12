ee89ca4fad32584387b7e3f08a13eea675342893
asad-jcs <asad-js@jcs-kyoto.com>
2024-11-12 12:07:54 +0900

    adding first time

---
+++ AzCam/CameraForm.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace AzCam
+{
+    partial class CameraForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.buttonStartRecording = new System.Windows.Forms.Button();
+            this.buttonStopRecording = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(247, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(244, 407);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(497, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // buttonStartRecording
+            // 
+            this.buttonStartRecording.Location = new System.Drawing.Point(497, 41);
+            this.buttonStartRecording.Name = "buttonStartRecording";
+            this.buttonStartRecording.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartRecording.TabIndex = 2;
+            this.buttonStartRecording.Text = "Record";
+            this.buttonStartRecording.UseVisualStyleBackColor = true;
+            this.buttonStartRecording.Click += new System.EventHandler(this.buttonStartRecording_Click);
+            // 
+            // buttonStopRecording
+            // 
+            this.buttonStopRecording.Location = new System.Drawing.Point(497, 70);
+            this.buttonStopRecording.Name = "buttonStopRecording";
+            this.buttonStopRecording.Size = new System.Drawing.Size(75, 23);
+            this.buttonStopRecording.TabIndex = 3;
+            this.buttonStopRecording.Text = "Stop";
+            this.buttonStopRecording.UseVisualStyleBackColor = true;
+            this.buttonStopRecording.Click += new System.EventHandler(this.buttonStopRecording_Click);
+            // 
+            // CameraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStopRecording);
+            this.Controls.Add(this.buttonStartRecording);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "CameraForm";
+            this.Text = "AZCam";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button buttonStartRecording;
+        private System.Windows.Forms.Button buttonStopRecording;
+    }
+}
+
---
+++ AzCam/CameraForm.cs
@@ -0,0 +1,141 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AzCam
+{
+    public partial class CameraForm : Form
+    {
+        private VideoCapture capture;
+        private Thread cameraThread;
+        private VideoWriter writer;
+        private bool isCameraRunning;
+        private bool isRecording;
+        public CameraForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStartRecording_Click(object sender, EventArgs e)
+        {
+            if (!isCameraRunning)
+            {
+                cameraThread = new Thread(StartCamera);
+                cameraThread.Start();
+                isCameraRunning = true;
+            }
+
+            // Start recording
+            StartRecording();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!isCameraRunning)
+            {
+                // Start the camera capture in a separate thread
+                cameraThread = new Thread(StartCamera);
+                cameraThread.Start();
+                isCameraRunning = true;
+            }
+        }
+
+        private void StartCamera()
+        {
+            capture = new VideoCapture(0); // 0 is the default camera
+            capture.Open(0);
+
+            if (!capture.IsOpened())
+            {
+                MessageBox.Show("Camera could not be opened.");
+                return;
+            }
+
+            // Set the capture resolution
+            capture.Set(VideoCaptureProperties.FrameWidth, 1080);
+            capture.Set(VideoCaptureProperties.FrameHeight, 1920);
+
+            while (isCameraRunning)
+            {
+                using (var frame = new Mat())
+                {
+                    // Capture the frame
+                    capture.Read(frame);
+
+                    if (frame.Empty())
+                        continue;
+
+                    // Rotate the frame 90 degrees clockwise
+                    Cv2.Rotate(frame, frame, RotateFlags.Rotate90Clockwise);
+
+                    // Display the frame in PictureBox
+                    Bitmap image = frame.ToBitmap();
+                    pictureBox1.Image?.Dispose();
+                    pictureBox1.Image = image;
+
+                    // Write to the video file if recording
+                    if (isRecording)
+                    {
+                        writer.Write(frame);
+                    }
+                }
+            }
+        }
+
+
+        private void StartRecording()
+        {
+            if (isRecording) return;
+
+            // Define the video file path
+            string filePath = "C:\\Video\\recorded_video.mp4";
+
+            // Ensure the directory exists
+            string directory = Path.GetDirectoryName(filePath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            // Initialize the VideoWriter with resolution 1080x1920, 30 fps
+            writer = new VideoWriter(filePath, FourCC.MJPG, 30, new OpenCvSharp.Size(1080, 1920));
+
+            if (!writer.IsOpened())
+            {
+                MessageBox.Show("Could not open the video file for writing.");
+                return;
+            }
+
+            isRecording = true;
+        }
+
+        private void buttonStopRecording_Click(object sender, EventArgs e)
+        {
+            // Stop recording
+            isRecording = false;
+
+            writer?.Release();
+            writer = null;
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Stop the camera capture when closing the form
+            isCameraRunning = false;
+            cameraThread?.Join();
+
+            capture?.Release();
+            base.OnFormClosing(e);
+        }
+    }
+}
